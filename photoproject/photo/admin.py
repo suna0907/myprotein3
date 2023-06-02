7ee29d47a594cd4507f49bf1bb661416edf90374
sunayama <Masaki_Sunayama@totec.co.jp>
2023-06-02 16:04:37 +0900

    p431 じっこうできない

--- photoproject/photo/admin.py
+++ photoproject/photo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, PhotoPost
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display=('id' , 'title')
+    list_display_links = ('id' , 'title')
+
+class PhotoPostAdmin(admin.ModelAdmin):
+    list_display=('id' , 'title')
+    list_display_links = ('id' , 'title')
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(PhotoPost, PhotoPostAdmin)
