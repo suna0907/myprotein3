3fba685c086ede70522ab8417cbed548e83cba89
sunayama <Masaki_Sunayama@totec.co.jp>
2023-06-02 15:26:26 +0900

    7.3 sqlite3,password_reset,password_reset,password_reset_form,models

--- photoproject/photo/models.py
+++ photoproject/photo/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from accounts.models import CustomUser
+
+class Category(models.Model):
+    title=models.CharField(
+        verbose_name='カテゴリ',
+        max_length=20
+    )
+
+def __str__(self):
+
+    return self.title
+
+class PhotoPost(models.Model):
+    user=models.ForeignKey(
+        CustomUser,
+        verbose_name='ユーザー',
+        on_delete=models.CASCADE
+    )
+
+category=models.ForeignKey(
+    Category,
+    verbose_name='カテゴリ',
+    on_delete=models.PROTECT
+)
+
+title=models.CharField(
+    verbose_name='タイトル',
+    max_length=200
+)
+
+comment=models.TextField(
+    verbose_name='コメント'
+)
+
+image1=models.ImageField(
+    verbose_name='イメージ1',
+    upload_to='photos'
+)
+
+image2=models.ImageField(
+    verbose_name='イメージ2',
+    upload_to='photos',
+    blank=True,
+    null=True
+)
+
+posted_at=models.DateTimeField(
+    verbose_name='投稿日時',
+    auto_now_add=True
+)
+
+def __str__(self):
+    return self.title
 
 # Create your models here.
