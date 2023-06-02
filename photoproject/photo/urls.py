85908d55a3add7d6f07a58253336818b4476a551
HIRAGA <fuya_hiraga@totec.co.jp>
2023-06-02 13:50:35 +0900

    7-4_P450_photo/uls.py

--- photoproject/photo/urls.py
+++ photoproject/photo/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 
     path('post/', views.CreatePhotoView.as_view(), name='post'),
+
+    path('post_done/',
+         views.PostSuccessView.as_view(),
+         name='post_done'),
 ]
