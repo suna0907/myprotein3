029a1a05a0f863687074d1936151a0e02f450d50
sunayama <Masaki_Sunayama@totec.co.jp>
2023-06-02 13:55:44 +0900

    7.2章 login,setting,password_reset,password_reset_done

--- photoproject/photoproject/settings.py
+++ photoproject/photoproject/settings.py
@@ -124,3 +124,20 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#P412で追加
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL='xxxxxxx@gmail.com'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='xxxx@gmail.com'
+EMAIL_HOST_PASSWORD='xxxxxxxxxxxxxxx'
+EMAIL_USE_TLS=True
+
+#p428で追加
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
+
